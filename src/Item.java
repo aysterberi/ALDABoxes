77d67af6bf7744ab570b6807d49529a558b79ec1
aysterberi <filipoppel@hotmail.com>
2016-03-04 17:07:44 +0100

    Some initiating methods

--- src/Item.java
+++ src/Item.java
@@ -1,10 +1,26 @@
 /**
  * Created by Joakim on 2016-03-02.
  */
-public class Item {
+public class Item implements Comparable<Item> {
     private int weight;
 
     public Item(int weight) {
         this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Item other) {
+        if(this.weight < other.weight) {
+            return -1;
+        } else if(this.weight > other.weight) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "" + this.weight;
     }
 }
--- src/Program.java
+++ src/Program.java
@@ -1,12 +1,34 @@
 import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Random;
 
-/**
- * Created by Joakim on 2016-03-02.
- */
 public class Program {
     private ArrayList<Box> boxes = null;
+    private PriorityQueue<Item> itemHeap = new PriorityQueue<>();
+    private Random rnd = new Random();
+    private int boxCap;
+
+    private void populateHeap() {
+        for(int i = 0; i < 10; i++) {
+            int num = rnd.nextInt(10) + 1;
+            Item item = new Item(num);
+            itemHeap.add(item);
+        }
+        System.out.println(itemHeap);
+    }
+
+    private void setBoxCapacity() {
+        boxCap = rnd.nextInt(10) + 1;
+    }
+
+    private void createBox() {
+        Box box = new Box(boxCap);
+        boxes.add(box);
+    }
 
     public static void main(String[] args) {
-
+        Program program = new Program();
+        program.setBoxCapacity();
+        program.populateHeap();
     }
 }
